551af2bd4a8f448e47aeff1797269b45811591ef
mjfmax <markfergusson@gmail.com>
2019-08-19 21:13:21 +0100

    Update test1.sh

--- test1.sh
+++ test1.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
-exit 0
 # added this comment #
 # This line is new on 19/08/2019
-mail -s "`hostname` Run update cron" markfergusson@gmail.com < /dev/null
+mail -s "`hostname` Hello Amanda" markfergusson@gmail.com < /dev/null
 exit 0
