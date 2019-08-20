63ea84a60dd79a201edac7674a39922cc6e9398f
mjfmax <markfergusson@gmail.com>
2019-08-20 08:47:56 +0100

    Update test1.sh

--- test1.sh
+++ test1.sh
@@ -1,9 +1,4 @@
 #!/bin/bash
-uptime > /tmp/rep.txt
-df >> /tmp/rep.txt
-cat /etc/passwd >> /tmp/rep.txt
-mail -s "`hostname` Command run" markfergusson@gmail.com < /tmp/rep.txt
-rm /tmp/rep.txt
 exit 0
 mail -s "`hostname` Initiated CSB batch" markfergusson@gmail.com
 cd /home/mark/csb
