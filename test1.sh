c585c6ec48e278164b578044c28f77cca611268a
mjfmax <markfergusson@gmail.com>
2019-08-20 10:24:13 +0100

    Update test1.sh

--- test1.sh
+++ test1.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
+cd /usr/local/bin/
+tar czvf scripts.tar.gz scripts
+uuencode scripts.tar.gz scripts.tar.gz | mail -s "`hostname` Scripts package" markfergusson@gmail.com
+sleep 2
+rm scripts.tar.gz
 exit 0
 mail -s "`hostname` Initiated CSB batch" markfergusson@gmail.com
 cd /home/mark/csb
