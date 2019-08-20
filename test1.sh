f5529af0c672748e687ab9eb04da55adab11ce0e
mjfmax <markfergusson@gmail.com>
2019-08-20 07:24:13 +0100

    Update test1.sh

--- test1.sh
+++ test1.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
-mail -s "`hostname` Good Morning World" markfergusson@gmail.com
-sleep 5
+exit 0
 mail -s "`hostname` Initiated CSB batch" markfergusson@gmail.com
 cd /home/mark/csb
 nohup ./doit.sh &
