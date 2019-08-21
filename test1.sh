3a3fa557b68a78f1dedfbf293a96febf59ae69a0
mjfmax <markfergusson@gmail.com>
2019-08-21 09:22:24 +0100

    Update test1.sh

--- test1.sh
+++ test1.sh
@@ -1,23 +1,31 @@
 #!/bin/bash
-exit 0
 MR=markfergusson@gmail.com
 SL=/usr/local/bin
 
+CSB ()
+{
+cd /home/mark/csb
+nohup ./doit.sh &
+}
+
+ZERO ()
+{
+mail -s "`hostname` ZERO file creation initiated" markfergusson@gmail.com < /dev/null
+cd /data3
+rm zero*.txt
+dd if=/dev/zero of=zero.txt count=5000000
+mail -s "`hostname` ZERO file creation completed" markfergusson@gmail.com < /dev/null
+}
+
+GETSCRIPTS ()
+{
 cd /usr/local/bin/
-uuencode scripts.tar.gz scripts.tar.gz | mail -s "`hostname` Scripts package" markfergusson@gmail.com
-exit 0
-ls -l scripts.tar.gz > /tmp/rep.txt
-mail -s "`hostname` tar file rep" markfergusson@gmail.com < /tmp/rep.txt
-exit 0
 tar czvf scripts.tar.gz scripts
-mail -s "Scripts Package" -t markfergusson@gmail.com -A scripts.tar.gz < /dev/null
-exit 0
 uuencode scripts.tar.gz scripts.tar.gz | mail -s "`hostname` Scripts package" markfergusson@gmail.com
 sleep 2
 rm scripts.tar.gz
-exit 0
-mail -s "`hostname` Initiated CSB batch" markfergusson@gmail.com
-cd /home/mark/csb
-nohup ./doit.sh &
-exit 0
-# dd if=/dev/zero of=zero7.txt count=5000000
+}
+
+CSB
+ZERO
+GETSCRIPTS
