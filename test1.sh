aeba7c223cfe10821df0223285b7e07288c62c0a
mjfmax <markfergusson@gmail.com>
2020-03-09 13:50:09 +0000

    Update test1.sh

--- test1.sh
+++ test1.sh
@@ -49,7 +49,7 @@ ZEROCLEAN ()
 {
 ls -lh /data1/zero* > /tmp/zerofiles.txt
 wc -l /data1/zero1.txt >> /tmp/zerofiles.txt
-wc -l /data1/zero3.txt >> /tmp/zerofiles.txt
+wc -l /data3/zero3.txt >> /tmp/zerofiles.txt
 rm /data1/zero*.txt
 mail -s "`hostname` ZERO file info" $MR < /tmp/zerofiles.txt
 }
