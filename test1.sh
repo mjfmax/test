d91372068cdaaf6e8f39103666691cf56645b451
mjfmax <markfergusson@gmail.com>
2020-03-11 08:38:44 +0000

    Update test1.sh

--- test1.sh
+++ test1.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-exit 0
+#exit 0
 MR=markfergusson@gmail.com
 SL=/usr/local/bin
 
@@ -104,13 +104,13 @@ HEALTH ()
 }
 
 #RMDOT
-CSB
+#CSB
 #CSB1
 #CSB2
 #CSB3
 #CSB4
 #ZEROCLEAN
-#ZERO1
+ZERO1
 #ZERO3
 HEALTH
 #GETSCRIPTS
