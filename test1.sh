796adc20f17c520ed7394c1de8a55a252aa9690b
Mark Fergusson <markfergusson@gmail.com>
2020-03-12 10:12:38 +0000

    multipl jobs enabled

--- test1.sh
+++ test1.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-exit 0
+#exit 0
 MR=markfergusson@gmail.com
 SL=/usr/local/bin
 
@@ -105,15 +105,15 @@ HEALTH ()
 
 #RMDOT
 #CSB
-#CSB1
-#CSB2
+CSB1
+CSB2
 #CSB3
 #CSB4
 #ZEROCLEAN
-#ZERO1
+ZERO1
 #ZERO3
-#HEALTH
-#GETSCRIPTS
-#PRM1PY
+HEALTH
+GETSCRIPTS
+PRM1PY
 #PRM2PY
 #GETPIC
