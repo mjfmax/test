da9acd556632a60667534c310e70b3df8abab82e
Mark Fergusson <markfergusson@gmail.com>
2020-03-17 08:44:14 +0000

    disable all

--- test1.sh
+++ test1.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#exit 0
+exit 0
 MR=markfergusson@gmail.com
 SL=/usr/local/bin
 
@@ -113,7 +113,7 @@ HEALTH ()
 #ZEROCLEAN
 #ZERO1
 #ZERO3
-HEALTH
+#HEALTH
 #GETSCRIPTS
 #PRM1PY
 #PRM2PY
