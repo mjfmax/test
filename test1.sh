a1ee033e204cfbc3b6a195619ec918c2945b553d
mjfmax <markfergusson@gmail.com>
2020-10-05 20:48:07 +0100

    Update test1.sh

--- test1.sh
+++ test1.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-exit 0
+#exit 0
 MR=markfergusson@gmail.com
 SL=/usr/local/bin
 
@@ -128,7 +128,7 @@ HEALTH ()
 #ZEROCLEAN
 #ZERO1
 #ZERO3
-#HEALTH
+HEALTH
 #GETCRON
 #GETSCRIPTS
 #PRM1PY
